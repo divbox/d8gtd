288c79cb8c9d6978fa330e3d2b489f56f4881916
John Divney <div@divbox.com>
2018-05-31 18:59:46 -0400

    initial commit on GTD module

---
+++ web/modules/custom/gtd/src/Controller/DivboxController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\gtd\Controller;
+
+class DivboxController
+{
+    public function hello()
+    {
+        return [
+            '#title' => 'madikins',
+            '#markup' => 'and stellar'
+        ];
+    }
+}
